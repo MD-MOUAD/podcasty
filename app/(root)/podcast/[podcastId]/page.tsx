e0257ffb6b2d149d7ee324ab0620d1752eac0956
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2025-05-21 12:53:22 +0100

    Implement podcasting platform structure with layout components, sidebar navigation, and initial pages for home, create, discover, and profile. Add Prettier for code formatting and update Tailwind CSS configuration.

---
+++ app/(root)/podcast/[podcastId]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const PodcastDetails = ({ params }: { params: { podcastId: string } }) => {
+  console.log(params.podcastId);
+  return <p className="text-white-1">details for {params.podcastId}</p>;
+};
+
+export default PodcastDetails;
