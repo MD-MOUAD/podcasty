2e481a14738dd79ea799e0ffeb0cdf6a99bcccd2
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2025-05-21 23:41:07 +0100

    Implement podcast creation form with validation, including fields for title, description, and AI voice selection.

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -1,10 +1,166 @@
-import React from "react";
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { cn } from "@/lib/utils";
+import { Textarea } from "@/components/ui/textarea";
+
+const voiceCategories = [
+  "Conversational",
+  "Narrative",
+  "Educational",
+  "Motivational",
+  "Comedic",
+  "News/Informative",
+  "Professional",
+  "Soothing/Calm",
+  "Dramatic",
+  "Interview-style",
+  "Panel Discussion",
+  "ASMR",
+  "Tech-focused",
+  "Spiritual/Reflective",
+  "Youthful/Energetic",
+];
+
+const formSchema = z.object({
+  podcastTitle: z
+    .string()
+    .min(3, "Podcast title must be at least 3 characters.")
+    .max(100, "Podcast title must be at most 100 characters."),
+
+  podcastDescription: z
+    .string()
+    .min(10, "Description should be at least 10 characters.")
+    .max(1000, "Description can't exceed 1000 characters."),
+});
 
 const CreatePodcast = () => {
+  // Define the form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      podcastTitle: "",
+      podcastDescription: "",
+    },
+  });
+
+  async function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log(data);
+  }
+
   return (
-    <div>
+    <section className="mt-10 flex flex-col">
       <h1 className="text-20 font-bold text-white-1">Create Podcast</h1>
-    </div>
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="mt-12 flex w-full flex-col"
+        >
+          <div className="flex flex-col gap-[30px] border-b border-black-5 pb-10">
+            <FormField
+              control={form.control}
+              name="podcastTitle"
+              render={({ field }) => (
+                <FormItem className="flex flex-col gap-2.5">
+                  <FormLabel className="text-16 font-bold text-white-1">
+                    Title
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="input-class focus-visible:ring-offset-orange-1"
+                      placeholder="JSM Pro Podcast"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-white-1" />
+                </FormItem>
+              )}
+            />
+
+            <div className="flex flex-col gap-2.5">
+              <Label className="text-16 font-bold text-white-1">
+                Select AI Voice
+              </Label>
+
+              <Select>
+                <SelectTrigger
+                  className={cn(
+                    "text-16 w-full border-none bg-black-1 text-gray-1 focus-visible:ring-offset-orange-1",
+                  )}
+                >
+                  <SelectValue
+                    placeholder="Select AI Voice"
+                    className="placeholder:text-gray-1"
+                  />
+                </SelectTrigger>
+                <SelectContent className="text-16 border-none bg-black-1 font-bold text-white-1 focus:ring-orange-1">
+                  {voiceCategories.map((category) => (
+                    <SelectItem
+                      key={category}
+                      value={category}
+                      className="capitalize focus:bg-orange-1"
+                    >
+                      {category}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+
+            <FormField
+              control={form.control}
+              name="podcastDescription"
+              render={({ field }) => (
+                <FormItem className="flex flex-col gap-2.5">
+                  <FormLabel className="text-16 font-bold text-white-1">
+                    Description
+                  </FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="input-class focus-visible:ring-offset-orange-1"
+                      placeholder="Write a short podcast description"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-white-1" />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex flex-col pt-10">
+            <div className="mt-10 w-full">
+              <Button
+                type="submit"
+                className="text-16 w-full bg-orange-1 py-4 font-extrabold text-white-1 transition-all duration-500 hover:bg-black-1"
+              >
+                Submit & Publish Podcast
+              </Button>
+            </div>
+          </div>
+        </form>
+      </Form>
+    </section>
   );
 };
 
